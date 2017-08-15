74bfbd1e9577df31cc6cb658736c73fdcf0d2e27
Ayan Das <ayandas@Ayans-MBP-2.attlocal.net>
2017-08-15 10:47:40 -0400

    Modified success response for response POSTing API

--- pokemon-node/controllers/RatingController.js
+++ pokemon-node/controllers/RatingController.js
@@ -15,14 +15,6 @@ module.exports.postRatings = function(req, res, next) {
         if (!docs) {
             return res.status(400).send({ 'error': 'Post did not work' });
         }
-        Rating.find(function(err, ratings) {
-            if (err) {
-                return res.status(500).send({ 'error': 'Could not get all ratings' });
-            }
-            if (!ratings) {
-                return res.status(400).send({ 'error': 'Could not find the ratings in the database' });
-            }
-            return res.status(200).send(ratings);
-        })
+        res.status(200).send({ 'success': 'Ratings were successfully posted' });
     })
 }
